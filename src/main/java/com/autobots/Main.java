a6f09cfa2fe4a8ffcf5de59620224288d9bf3eb8
ZinaidaMamaiusupova <begmetovazinaida@rambler.ru>
2025-07-08 08:08:15 +0600

    Update Main.java

--- src/main/java/com/autobots/Main.java
+++ src/main/java/com/autobots/Main.java
@@ -9,7 +9,8 @@ public class Main {
         System.out.printf("Hello and welcome!");
 // example
  // example 2
-// git fetch java        
+// git fetch java     
+        // git pull practics
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
